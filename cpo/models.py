eaeb699ce31335f633756c722ad45eab5d4d349e
gabrielandrade131 <gabrielandrade131@gmail.com>
2025-05-08 14:13:22 -0300

    chore: adicionando o models

--- cpo/models.py
+++ cpo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    matricula = models.CharField(max_length=50)
+    funcao = models.CharField(max_length=255)
+    competencia = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
