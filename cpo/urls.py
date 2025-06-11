6007a92072b92cd6365c8d68e68e3e7ae95b3f4e
gabrielandrade131 <gabrielandrade131@gmail.com>
2025-06-11 14:05:43 -0300

    fix: correção no banco de dados e na tela de login

--- cpo/urls.py
+++ cpo/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 from cpo import views
 from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView
+from .forms import CPFLoginForm
 
 urlpatterns = [
     path('', lambda request: redirect('login'), name='home'),
     path('salvar-folha-ponto/', views.salvar_folha_ponto, name='salvar_folha_ponto'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('login/', LoginView.as_view(
+        template_name='registration/login.html',
+        authentication_form=CPFLoginForm
+    ), name='login'),
+
     path('', include('django.contrib.auth.urls')),
 ]
